eb6da4168c73b8160ee494d6892a18c6fcedd1b3
jfnever <jfnever@hotmail.com>
2016-05-31 16:14:13 +0800

    MUGGLEWORKS
    index html
    local storage

---
+++ js/js1.js
@@ -0,0 +1,16 @@
+if (localStorage.pagecount)
+{
+    localStorage.pagecount=Number(localStorage.pagecount) +1;
+}
+else
+{
+    localStorage.pagecount=1;
+}
+
+var countWrite = document.getElementById("count1");
+
+countWrite.innerHTML = localStorage.pagecount;
+
+//window.onload = function(){
+   // $("#count1").val(localStorage.pagecount);
+//};
